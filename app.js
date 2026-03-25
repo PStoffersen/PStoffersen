900e08d23ce260602af52fd655410fae0a4debc4
PStoffersen <110452928+PStoffersen@users.noreply.github.com>
2026-03-25 15:08:39 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+window.APP_CONFIG = {
+  // Indsæt URL'en til dit deployede Google Apps Script Web App her
+  API_URL: 'https://script.google.com/macros/s/AKfycbze2Dk69HFI2lUGrqvMcClje1CCpEebXyuc5747VmTk_kTriE2x20fwangsgbaWRbMt/exec'
+};
